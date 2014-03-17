353ee4f9fd2f68bab49dba1eafb6f1075ad66cb5
katherinewallace <kwalla89@gmail.com>
2014-03-17 12:07:53 -0400

    Update router.rb

--- lib/rails_lite/router.rb
+++ lib/rails_lite/router.rb
@@ -12,7 +12,7 @@ class Route
     @action_name = action_name
   end
 
-  # checks if pattern matches path and method matches request method
+
   def matches?(req)
     self.pattern.match(req.path) && 
     (@http_method == req.request_method.downcase.to_sym)
@@ -37,26 +37,23 @@ class Router
     @routes = []
   end
 
-  # simply adds a new route to the list of routes
+
   def add_route(pattern, method, controller_class, action_name)
     @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
-  # evaluate the proc in the context of the instance
-  # for syntactic sugar :)
   def draw(&proc)
     self.instance_eval(&proc)
   end
 
-  # make each of these methods that
-  # when called add route
+
   [:get, :post, :put, :delete].each do |http_method|
     define_method(http_method) do |pattern, controller_class, action_name|
       add_route(pattern, http_method, controller_class, action_name)
     end
   end
 
-  # should return the route that matches this request
+
   def match(req)
     @routes.each do |route|
       return route if route.matches?(req)
@@ -64,7 +61,7 @@ class Router
     nil
   end
 
-  # either throw 404 or call run on a matched route
+
   def run(req, res)
     
     route = self.match(req)
