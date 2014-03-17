73a326534b3c8aba922bf23056ee8241ff088d02
katherinewallace <kwalla89@gmail.com>
2014-03-17 12:34:45 -0400

    Update controller_base.rb

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -7,7 +7,6 @@ require_relative 'session'
 class ControllerBase
   attr_reader :params, :req, :res
 
-  # setup the controller
   def initialize(req, res, route_params = {})
     @req = req
     @res = res
@@ -15,9 +14,6 @@ class ControllerBase
     @params = Params.new(req, route_params)
   end
 
-  # populate the response with content
-  # set the responses content type to the given type
-  # later raise an error if the developer tries to double render
   def render_content(content, type)
     raise "already rendered" if already_rendered?
     @res.content_type = type
@@ -26,12 +22,10 @@ class ControllerBase
     @already_rendered = true
   end
 
-  # helper method to alias @already_rendered
   def already_rendered?
     @already_rendered
   end
 
-  # set the response status code and header
   def redirect_to(url)
     raise "already rendered" if already_rendered?
     @res.header["location"] = url
@@ -40,20 +34,16 @@ class ControllerBase
     @already_rendered = true
   end
 
-  # use ERB and binding to evaluate templates
-  # pass the rendered html to render_content
   def render(template_name)
     contents = File.read("views/#{self.class.to_s.underscore}/#{template_name}.html.erb")
     template = ERB.new(contents).result(binding)
     render_content(template, "text/html")
   end
 
-  # method exposing a `Session` object
   def session
     @session ||= Session.new(req)
   end
 
-  # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
     self.send(name.to_sym)
     unless already_rendered?
