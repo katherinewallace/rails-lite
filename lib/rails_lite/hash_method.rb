dc248c6317c9cdac9779cdd17886d7975e9525b0
Katherine Wallace <kwalla89@gmail.com>
2014-03-20 18:30:50 -0400

    Minor reorganization

--- lib/rails_lite/hash_method.rb
+++
@@ -1,6 +0,0 @@
-module HashHelper
-  def hash_deep_merge(first, second)
-      merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
-      first.merge(second, &merger)
-  end
-end
--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -38,6 +38,11 @@ class Params
     key.gsub("]", "").split("[")
   end
   
-
+  private
+  
+  def hash_deep_merge
+    merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+    first.merge(second, &merger)
+  end
   
 end
